9ae72cba0bfbba86009a4e7c24e5bc0cda3ec733
Igor Emelyanov <igor3melya@yandex.ru>
2025-05-12 15:22:09 +0300

    realization of the function of adding cards from page loading and deleting them

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardsContainer = document.querySelector('.places__list');
+
+function addCard(cardContent, deleteCard) {
+  const card = document.querySelector('#card-template').content.querySelector('.card').cloneNode(true);
+  const cardImage = card.querySelector('.card__image');
+  const cardTitle = card.querySelector('.card__title');
+  cardImage.src = cardContent.link;
+  cardImage.alt = cardContent.name;
+  cardTitle.textContent = cardContent.name;
+  card.querySelector('.card__delete-button').addEventListener('click', function() {
+    deleteCard(card);
+  });
+  return card;
+};
+
+function deleteCard(card) {
+  card.remove();
+};
+
+initialCards.forEach(function (cardContent) {
+  cardsContainer.append(addCard(cardContent, deleteCard));
+});
